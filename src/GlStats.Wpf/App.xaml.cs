0ac0097e7ddc9cb0a44efd7776df027f1789c1ab
Alex <sepp.amsl@protonmail.com>
2022-05-05 22:07:00 +0200

    Project base.

---
+++ src/GlStats.Wpf/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using GlStats.Wpf.Views;
+
+namespace GlStats.Wpf;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : PrismApplication
+{
+    protected override Window CreateShell()
+    {
+        var w = Container.Resolve<MainWindow>();
+        return w;
+    }
+
+    protected override void RegisterTypes(IContainerRegistry container)
+    {
+
+    }
+}
+
---
+++ src/GlStats.Wpf/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿global using System.Windows;
+global using Prism.DryIoc;
+global using Prism.Ioc;
+global using Prism.Mvvm;
+global using Prism.Commands;
